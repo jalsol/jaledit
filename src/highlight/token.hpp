a1e14481c6f718264bb58c192623a44164ad6cb0
jalsol <jalsol@pm.me>
2023-07-26 23:27:11 +0700

    Additional support for syntax highlight

--- src/highlight/token.hpp
+++ src/highlight/token.hpp
@@ -7,6 +7,12 @@
 #include <string>
 #include <string_view>
 
+namespace constants {
+
+constexpr std::size_t token_count = 21;
+
+} // namespace constants
+
 enum class TokenKind : std::size_t {
     End,
     Invalid,
@@ -16,17 +22,22 @@ enum class TokenKind : std::size_t {
     CloseParen,
     OpenCurly,
     CloseCurly,
+    OpenSquare,
+    CloseSquare,
     OpenAttr,
     CloseAttr,
     Semicolon,
     Keyword,
     Comment,
     String,
-    Class,
+    Char,
+    Type,
     Number,
+    Function,
+    Operator,
 };
 
-constexpr std::array<std::string_view, 16> kind_names = {{
+constexpr std::array<std::string_view, constants::token_count> kind_names = {{
     "end",
     "invalid",
     "preproc",
@@ -35,38 +46,49 @@ constexpr std::array<std::string_view, 16> kind_names = {{
     "close_paren",
     "open_curly",
     "close_curly",
+    "open_square",
+    "close_square"
     "open_attr",
     "close_attr",
     "semicolon",
     "keyword",
     "comment",
     "string",
-    "class",
+    "char",
+    "type",
     "number",
+    "function",
+    "operator",
 }};
 
-constexpr std::array<Color, 16> kind_colors = {{
+constexpr std::array<Color, constants::token_count> kind_colors = {{
     {0, 0, 0, 0},        // End
     {76, 79, 105, 255},  // Invalid
     {23, 146, 153, 255}, // Preproc
     {76, 79, 105, 255},  // Symbol
 
-    {254, 100, 11, 255}, // OpenParen
-    {254, 100, 11, 255}, // CloseParen
+    {223, 142, 29, 255}, // OpenParen
+    {223, 142, 29, 255}, // CloseParen
 
-    {254, 100, 11, 255}, // OpenCurly
-    {254, 100, 11, 255}, // CloseCurly
+    {223, 142, 29, 255}, // OpenCurly
+    {223, 142, 29, 255}, // CloseCurly
 
-    {254, 100, 11, 255}, // OpenAttr
-    {254, 100, 11, 255}, // CloseAttr
+    {223, 142, 29, 255}, // OpenSquare
+    {223, 142, 29, 255}, // CloseSquare
+
+    {223, 142, 29, 255}, // OpenAttr
+    {223, 142, 29, 255}, // CloseAttr
 
     {23, 146, 153, 255}, // Semicolon
 
     {230, 69, 83, 255},   // Keyword
     {188, 192, 204, 255}, // Comment
     {64, 160, 43, 255},   // String
-    {254, 100, 11, 255},  // Class
+    {64, 160, 43, 255},   // Char
+    {254, 100, 11, 255},  // Type
     {254, 100, 11, 255},  // Number
+    {114, 135, 253, 255}, // Function
+    {4, 165, 229, 255},   // Operator
 }};
 
 class Token {
