1d2def8974bc49f2427c428396758785338dceb8
jalsol <jalsol@pm.me>
2023-08-24 02:33:03 +0700

    Remove shared pointer
    Refcount will be implemented directly inside the nodes

--- src/common/shared_ptr.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct SharedPtr;
-typedef struct SharedPtr SharedPtr;
-
-SharedPtr *shared_ptr_new(void *ptr);
-void shared_ptr_delete(SharedPtr *shared_ptr);
-void *shared_ptr_get(SharedPtr *shared_ptr);
-SharedPtr *shared_ptr_clone(SharedPtr *shared_ptr);
