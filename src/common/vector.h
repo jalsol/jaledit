5505ac5eb64df623d4f67710fdd11e7c1f91ea5f
jalsol <jalsol@pm.me>
2023-08-23 17:10:47 +0700

    Un-capitalize Vec function names

--- src/common/vector.h
+++ src/common/vector.h
@@ -7,19 +7,19 @@
     struct Vec_##T;                                                                      \
     typedef struct Vec_##T Vec_##T;                                                      \
                                                                                          \
-    struct Vec_##T *Vec_##T##_new();                                                     \
-    void Vec_##T##_delete(struct Vec_##T *vec);                                          \
-    void Vec_##T##_push_back(struct Vec_##T *vec, T value);                              \
-    void Vec_##T##_pop_back(struct Vec_##T *vec);                                        \
-    T Vec_##T##_back(struct Vec_##T *vec);                                               \
-    T Vec_##T##_at(struct Vec_##T *vec, size_t index);                                   \
-    size_t Vec_##T##_size(struct Vec_##T *vec);                                          \
-    size_t Vec_##T##_capacity(struct Vec_##T *vec);                                      \
-    T *Vec_##T##_data(struct Vec_##T *vec);                                              \
-    void Vec_##T##_set(struct Vec_##T *vec, size_t index, T value);                      \
-    void Vec_##T##_reserve(struct Vec_##T *vec, size_t capacity);                        \
-    void Vec_##T##_resize(struct Vec_##T *vec, size_t size);                             \
-    struct Vec_##T *Vec_##T##_new_subvec(struct Vec_##T *vec, size_t start, size_t end);
+    struct Vec_##T *vec_##T##_new();                                                     \
+    void vec_##T##_delete(struct Vec_##T *vec);                                          \
+    void vec_##T##_push_back(struct Vec_##T *vec, T value);                              \
+    void vec_##T##_pop_back(struct Vec_##T *vec);                                        \
+    T vec_##T##_back(struct Vec_##T *vec);                                               \
+    T vec_##T##_at(struct Vec_##T *vec, size_t index);                                   \
+    size_t vec_##T##_size(struct Vec_##T *vec);                                          \
+    size_t vec_##T##_capacity(struct Vec_##T *vec);                                      \
+    T *vec_##T##_data(struct Vec_##T *vec);                                              \
+    void vec_##T##_set(struct Vec_##T *vec, size_t index, T value);                      \
+    void vec_##T##_reserve(struct Vec_##T *vec, size_t capacity);                        \
+    void vec_##T##_resize(struct Vec_##T *vec, size_t size);                             \
+    struct Vec_##T *vec_##T##_new_subvec(struct Vec_##T *vec, size_t start, size_t end);
 
 DECLARE_VEC(char)
 DECLARE_VEC(int)
--- src/common/vector_test.c
+++ src/common/vector_test.c
@@ -3,43 +3,43 @@
 
 void print_vec(Vec_char *vec) {
     printf("vec: ");
-    for (size_t i = 0; i < Vec_char_size(vec); i++) {
-        printf("%c", Vec_char_at(vec, i));
+    for (size_t i = 0; i < vec_char_size(vec); i++) {
+        printf("%c", vec_char_at(vec, i));
     }
     printf("\n");
 }
 
 int main(void) {
-    Vec_char *vec = Vec_char_new();
-    Vec_char_push_back(vec, 'a');
-    Vec_char_push_back(vec, 'b');
-    Vec_char_push_back(vec, 'c');
+    Vec_char *vec = vec_char_new();
+    vec_char_push_back(vec, 'a');
+    vec_char_push_back(vec, 'b');
+    vec_char_push_back(vec, 'c');
 
     print_vec(vec);
 
-    Vec_char_pop_back(vec);
+    vec_char_pop_back(vec);
 
     print_vec(vec);
-    printf("back: %c\n", Vec_char_back(vec));
-    printf("at: %c\n", Vec_char_at(vec, 0));
-    printf("size: %zu\n", Vec_char_size(vec));
-    printf("capacity: %zu\n", Vec_char_capacity(vec));
+    printf("back: %c\n", vec_char_back(vec));
+    printf("at: %c\n", vec_char_at(vec, 0));
+    printf("size: %zu\n", vec_char_size(vec));
+    printf("capacity: %zu\n", vec_char_capacity(vec));
 
-    Vec_char_push_back(vec, '\0');
-    printf("data: %s\n", Vec_char_data(vec));
+    vec_char_push_back(vec, '\0');
+    printf("data: %s\n", vec_char_data(vec));
 
-    Vec_char_set(vec, 0, 'd');
+    vec_char_set(vec, 0, 'd');
     print_vec(vec);
 
-    Vec_char_reserve(vec, 10);
-    printf("capacity: %zu\n", Vec_char_capacity(vec));
+    vec_char_reserve(vec, 10);
+    printf("capacity: %zu\n", vec_char_capacity(vec));
 
-    Vec_char_resize(vec, 5);
-    printf("size: %zu\n", Vec_char_size(vec));
+    vec_char_resize(vec, 5);
+    printf("size: %zu\n", vec_char_size(vec));
 
-    Vec_char *subvec = Vec_char_new_subvec(vec, 1, 3);
+    Vec_char *subvec = vec_char_new_subvec(vec, 1, 3);
     print_vec(subvec);
 
-    Vec_char_delete(vec);
-    Vec_char_delete(subvec);
+    vec_char_delete(vec);
+    vec_char_delete(subvec);
 }
